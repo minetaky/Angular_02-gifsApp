1766b715ba67d3a1c76ea7324f9ad13c3b5669bc
Veronica M. Melendez Montiel <minetaky@gmail.com>
2022-03-17 17:01:28 -0600

    86. Mostrar los resultados harcodeados en pantalla

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,12 +7,17 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _historial:  string[] = [];
+  private _apiKey: string = 'Bh8qhx85rQR6tbRNVoO7h32OOCOgyfsv';
 
   get historial(){
     return [...this._historial];
   }
 
-  buscarGifs( query: string ){
+  constructor( private _http: HttpClient ){
+
+  }
+
+buscarGifs( query: string ){
     query = query.trim().toLocaleLowerCase();
 
     if( !this._historial.includes( query ) ){
@@ -19,7 +25,23 @@ export class GifsService {
       this._historial = this.historial.splice(0,10);
     }
 
-    console.log( this._historial );
+    //Forma 1 con promesas
+/*    fetch('https://api.giphy.com/v1/gifs/search?api_key=Bh8qhx85rQR6tbRNVoO7h32OOCOgyfsv&q=como%20conoci%20a%20tu%20madre&limit=10')
+   .then( resp => {
+     resp.json().then( data => { 
+      console.log( data );
+     } ) 
+   }) */
+
+   //Forma 2 mas resumida: se debe marcar la funcion como: async
+/*    const resp = await fetch('https://api.giphy.com/v1/gifs/search?api_key=Bh8qhx85rQR6tbRNVoO7h32OOCOgyfsv&q=como%20conoci%20a%20tu%20madre&limit=10')
+   const data = await resp.json();
+   console.log( data ); */
+
+   this._http.get('https://api.giphy.com/v1/gifs/search?api_key=Bh8qhx85rQR6tbRNVoO7h32OOCOgyfsv&q=como%20conoci%20a%20tu%20madre&limit=10')
+   .subscribe( resp => {
+     console.log( resp );
+   });
 
   }
 
